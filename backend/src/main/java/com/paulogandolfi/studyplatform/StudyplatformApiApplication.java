ab157d9320ac5839c802852384714cf87c93c58a
paulogandolfi <paulo_gandolfi@hotmail.com>
2026-05-11 10:10:43 -0300

    Initial commit

---
+++ backend/src/main/java/com/paulogandolfi/studyplatform/StudyplatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.paulogandolfi.studyplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyplatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyplatformApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/paulogandolfi/studyplatform/StudyplatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulogandolfi.studyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyplatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
